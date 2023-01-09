115b32f26de30632b076078e9913b24fec53f893
pbassham <pbassham@gmail.com>
2023-01-09 15:33:40 -0600

    clean up action. only update slug if title changed

--- index.js
+++ index.js
@@ -1,33 +1,27 @@
 const core = require("@actions/core")
 const github = require("@actions/github")
 
-// run()
-// async function run() {
 try {
-  // `who-to-greet` input defined in action metadata file
-  // const nameToGreet = core.getInput("who-to-greet")
-  // const issue_title = core.getInput("issue_title")
-
   // This should be triggerd with the issue, so will have that payload
-  const { issue } = github.context.payload
+  const { action, issue, changes } = github.context.payload
   // const account = core.getInput("cloudflare_account_id")
   // const namespace = core.getInput("cloudflare_namespace_id")
   // const token = core.getInput("cloudflare_token")
-  // console.log(`Hello ${nameToGreet}!`)
-  // console.log(`Title: ${issue_title}!`)
   const slug = slugify(issue?.title)
-
   console.log(`Title: ${issue.title}!`)
   console.log(`Slug: ${slug}!`)
+  const updateKey = true
 
-  // const res = await updateKey(slug, issue.number, account, namespace, token)
+  if (action === "edited") {
+    const oldSlug = changes?.title?.from
+    if (oldSlug && slug !== slugify(oldSlug)) {
+      updateKey = false
+    }
+  }
 
-  // console.log(res)
-  // const time = new Date().toTimeString()
-
-  // core.setOutput("time", time)
   core.setOutput("slug", slug)
   core.setOutput("issue_number", issue.number)
+  core.setOutput("needsUpdate", updateKey)
 
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
@@ -47,23 +41,3 @@ function slugify(text) {
     .replace(/[^\w\-]+/g, "") // Remove all non-word chars
     .replace(/\-\-+/g, "-") // Replace multiple - with single -
 }
-
-// async function updateKey(key, value, CLOUDFLARE_ACCOUNT_ID, CLOUDFLARE_NAMESPACE_ID, CLOUDFLARE_TOKEN) {
-//   const headers = {
-//     Authorization: `Bearer ${CLOUDFLARE_TOKEN}`,
-//     "Content-Type": "application/json",
-//   }
-//   const { success, result, errors } = await fetch(
-//     `https://api.cloudflare.com/client/v4/accounts/${CLOUDFLARE_ACCOUNT_ID}/storage/kv/namespaces/${CLOUDFLARE_NAMESPACE_ID}/values/${key}`,
-//     {
-//       method: "PUT",
-//       headers,
-//       body: value,
-//     }
-//   ).then((response) => response.json())
-
-//   console.log(success)
-//   console.log(result)
-//   console.log(errors)
-//   // ...
-// }
