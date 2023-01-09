8cea3543d0319b09f43557674663a631f4f4b523
pbassham <pbassham@gmail.com>
2023-01-09 15:08:50 -0600

    simplify action

--- index.js
+++ index.js
@@ -1,41 +1,41 @@
 const core = require("@actions/core")
 const github = require("@actions/github")
 
-run()
-async function run() {
-  try {
-    // `who-to-greet` input defined in action metadata file
-    // const nameToGreet = core.getInput("who-to-greet")
-    // const issue_title = core.getInput("issue_title")
+// run()
+// async function run() {
+try {
+  // `who-to-greet` input defined in action metadata file
+  // const nameToGreet = core.getInput("who-to-greet")
+  // const issue_title = core.getInput("issue_title")
 
-    // This should be triggerd with the issue, so will have that payload
-    const { issue } = github.context.payload
-    const account = core.getInput("cloudflare_account_id")
-    const namespace = core.getInput("cloudflare_namespace_id")
-    const token = core.getInput("cloudflare_token")
-    // console.log(`Hello ${nameToGreet}!`)
-    // console.log(`Title: ${issue_title}!`)
-    const slug = slugify(issue?.title)
+  // This should be triggerd with the issue, so will have that payload
+  const { issue } = github.context.payload
+  // const account = core.getInput("cloudflare_account_id")
+  // const namespace = core.getInput("cloudflare_namespace_id")
+  // const token = core.getInput("cloudflare_token")
+  // console.log(`Hello ${nameToGreet}!`)
+  // console.log(`Title: ${issue_title}!`)
+  const slug = slugify(issue?.title)
 
-    console.log(`Title: ${issue.title}!`)
-    console.log(`Slug: ${slug}!`)
+  console.log(`Title: ${issue.title}!`)
+  console.log(`Slug: ${slug}!`)
 
-    // const res = await updateKey(slug, issue.number, account, namespace, token)
+  // const res = await updateKey(slug, issue.number, account, namespace, token)
 
-    // console.log(res)
-    const time = new Date().toTimeString()
+  // console.log(res)
+  // const time = new Date().toTimeString()
 
-    core.setOutput("time", time)
-    core.setOutput("slug", slug)
-    core.setOutput("issue_number", issue.number)
+  // core.setOutput("time", time)
+  core.setOutput("slug", slug)
+  core.setOutput("issue_number", issue.number)
 
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`)
-  } catch (error) {
-    core.setFailed(error.message)
-  }
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`)
+} catch (error) {
+  core.setFailed(error.message)
 }
+// }
 
 function slugify(text) {
   return text
@@ -48,22 +48,22 @@ function slugify(text) {
     .replace(/\-\-+/g, "-") // Replace multiple - with single -
 }
 
-async function updateKey(key, value, CLOUDFLARE_ACCOUNT_ID, CLOUDFLARE_NAMESPACE_ID, CLOUDFLARE_TOKEN) {
-  const headers = {
-    Authorization: `Bearer ${CLOUDFLARE_TOKEN}`,
-    "Content-Type": "application/json",
-  }
-  const { success, result, errors } = await fetch(
-    `https://api.cloudflare.com/client/v4/accounts/${CLOUDFLARE_ACCOUNT_ID}/storage/kv/namespaces/${CLOUDFLARE_NAMESPACE_ID}/values/${key}`,
-    {
-      method: "PUT",
-      headers,
-      body: value,
-    }
-  ).then((response) => response.json())
+// async function updateKey(key, value, CLOUDFLARE_ACCOUNT_ID, CLOUDFLARE_NAMESPACE_ID, CLOUDFLARE_TOKEN) {
+//   const headers = {
+//     Authorization: `Bearer ${CLOUDFLARE_TOKEN}`,
+//     "Content-Type": "application/json",
+//   }
+//   const { success, result, errors } = await fetch(
+//     `https://api.cloudflare.com/client/v4/accounts/${CLOUDFLARE_ACCOUNT_ID}/storage/kv/namespaces/${CLOUDFLARE_NAMESPACE_ID}/values/${key}`,
+//     {
+//       method: "PUT",
+//       headers,
+//       body: value,
+//     }
+//   ).then((response) => response.json())
 
-  console.log(success)
-  console.log(result)
-  console.log(errors)
-  // ...
-}
+//   console.log(success)
+//   console.log(result)
+//   console.log(errors)
+//   // ...
+// }
