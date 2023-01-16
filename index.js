376f7aacd39975959ba3d31bd573d1b1ad3ade09
pbassham <pbassham@gmail.com>
2023-01-16 17:45:37 -0600

    had logic backwards

--- index.js
+++ index.js
@@ -64,11 +64,11 @@ try {
 // }
 
 function slugify(text) {
-  console.log(text);
-  console.log(typeof text);
-  if (text && typeof text === "string") return null
+  // console.log(text);
+  // console.log(typeof text);
+  if (!text || typeof text !== "string") return null
   return text
-    .toString() // Cast to string (optional)
+    // .toString() // Cast to string (optional)
     .normalize("NFKD") // The normalize() using NFKD method returns the Unicode Normalization Form of a given string.
     .toLowerCase() // Convert the string to lowercase letters
     .trim() // Remove whitespace from both sides of a string (optional)
